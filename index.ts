70af7d739d81f91dba42a4506b870981e2b7637c
Philippe DeSousa <phil.desousa@gmail.com>
2026-05-13 11:55:00 -0400

    update index

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { Context, APIGatewayEvent } from 'aws-lambda';
 import { router } from './router';
 import { LambdaHandlerParams } from './types';
 
-export const lambdaHandler = (
+export const lambdaHandler = async (
     event: APIGatewayEvent,
     _context: Context,
-): void => {
+) => {
     const handlerParams: LambdaHandlerParams = {
         event,
     };
 
-    router(handlerParams);
+    return router(handlerParams);
 };
